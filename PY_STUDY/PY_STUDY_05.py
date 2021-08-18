557c63e26dfa5aa25da99606086d713214da4f96
jihunHong <48124955+HONG-C@users.noreply.github.com>
2021-08-18 14:25:56 +0000

    클래스 관련 설명 추가

--- PY_STUDY/PY_STUDY_05.py
+++ PY_STUDY/PY_STUDY_05.py
@@ -4,12 +4,19 @@
 *있는 문장 주석처리할 때
 컨트롤+슬래시(물음표)
 '''
+#부모 클래스 
 class drive_time:
     def __init__(self,angle,speed,time):
       self.angle=angle
       self.speed=speed
 
+#클래스 내의 함수=메서드
+#상속  
 class drive_time_A(drive_time):
     def __init__(self,angle,speed,time):
       drive_time.__init__(self,angle,speed,time)
       
+#메소드 생성
+    def attack(self,location):#메소드 앞에는 무조건 self 적기!
+      print("{0}:{1}방향으로 적군을 공격합니다.[공격력{2}]"\#\이거 쓰면 다음줄로 이어짐 
+    .format(self.name,location,self.damage))
