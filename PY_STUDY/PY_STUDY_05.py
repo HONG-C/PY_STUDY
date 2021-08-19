305597bffbd1e3920daf4d9f36b5d4b8f646d6b9
jihunHong <48124955+HONG-C@users.noreply.github.com>
2021-08-19 12:57:04 +0000

    클래스 관련 내용 추가 및 오류 수정

--- PY_STUDY/PY_STUDY_05.py
+++ PY_STUDY/PY_STUDY_05.py
@@ -9,7 +9,7 @@ class drive_time:
     def __init__(self,angle,speed,time):
       self.angle=angle
       self.speed=speed
-
+      self.time=time
 #클래스 내의 함수=메서드
 #상속  
 class drive_time_A(drive_time):
@@ -17,6 +17,6 @@ class drive_time_A(drive_time):
       drive_time.__init__(self,angle,speed,time)
       
 #메소드 생성
-    def attack(self,location):#메소드 앞에는 무조건 self 적기!
-      print("{0}:{1}방향으로 적군을 공격합니다.[공격력{2}]"\#\이거 쓰면 다음줄로 이어짐 
-    .format(self.name,location,self.damage))
+    def attack(self):#메소드 앞에는 무조건 self 적기!
+      print("조향각:{0}속도:{12시간:{2}"\#\이거 쓰면 다음줄로 이어짐 
+    .format(self.angle,self.speed,self.damage))
